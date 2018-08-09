dd57d72ca35f6826d0370db87d1a64b3e9de927a
albertpai <verdant.light@gmail.com>
2018-08-09 11:32:49 +0100

    Update content and import Bootstrap in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Albert Pai</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h1>Life of Pai</h1>
+          <h1>Tech interests and skills</h1>
+          <h1>Hobbies</h1>
+        </div>
       </div>
     );
   }
