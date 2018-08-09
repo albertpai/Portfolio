09541e86efddf5e5f5b4a55c741d01cb07dc7f56
albertpai <verdant.light@gmail.com>
2018-08-09 13:15:45 +0100

    Update content in App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,35 @@ class App extends Component {
           <h1 className="App-title">Albert Pai</h1>
         </header>
         <div className="App-intro">
-          <h1>Life of Pai</h1>
-          <h1>Tech interests and skills</h1>
+          <h1>About Life of Pai</h1>
+          <p>
+            As a junior developer, I am an avid learner of JavaScript and React. I believe that programming can help make a better world, and technology should be shared with the society.
+            </p>
+          <h1>Frontend Web Development Skills</h1>
+          <p>JavaScript</p>
+          <p>HTML5</p>
+          <p>CSS3</p>
+          <p>Bootstrap</p>
+          <p>React</p>
+          <p>Ruby</p>
+          <p>More Skills</p>
+          <p>Statistical software: SPSS</p>
+          <p>Qualitative analysis software: NVivo</p>
+          <p>Office software: Word, Excel and PowerPoint.</p>
+          <p>Desktop publishing software: Adobe Acrobat Pro.</p>
+          <p>Computer‐assisted translation tools: SDL Trados Studio and memoQ.</p>
+          <p>Cloud-based translation tools: MateCat, XTM Cloud, Memsource Cloud.</p>
           <h1>Hobbies</h1>
+          <p>
+            My hobbies include coding and web design. I also love exploring nature and listening to music.
+            </p>
+          <h1>Contact Me</h1>
+          <p>
+            Email
+            LinkedIn
+            Facebook
+            Twitter
+            </p>
         </div>
       </div>
     );
