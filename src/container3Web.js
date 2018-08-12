810aec815e9879f7775551058b0daf57b1f6b4c6
albertpai <verdant.light@gmail.com>
2018-08-12 02:07:58 +0100

    Add ContainerWeb component

---
+++ src/container3Web.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './container3Web.css';
+
+export default class ContainerWeb extends React.Component {
+    render() {
+        return (
+            <div className="container-web">
+                <h1 className="web-item1">Full-Stack Web Development Skills</h1>
+                <div className="web-item2">
+                    <i className="devicon-javascript-plain colored icon-skill"></i>
+                    <p>JavaScript</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-react-original colored icon-skill"></i>
+                    <p>React</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-html5-plain colored icon-skill"></i>
+                    <p>HTML5</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-css3-plain colored icon-skill"></i>
+                    <p>CSS3</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-bootstrap-plain colored icon-skill"></i>
+                    <p>Bootstrap</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-github-plain colored icon-skill"></i>
+                    <p>Github</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-git-plain-wordmark colored icon-skill"></i>
+                    <p>Git</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-heroku-plain colored icon-skill"></i>
+                    <p>Heroku</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-nodejs-plain-wordmark colored icon-skill"></i>
+                    <p>Node.js</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-express-original colored icon-skill"></i>
+                    <p>Express.js</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-mongodb-plain-wordmark colored icon-skill"></i>
+                    <p>MongoDB</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-ruby-plain colored icon-skill"></i>
+                    <p>Ruby</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-python-plain colored icon-skill"></i>
+                    <p>Python</p>
+                </div>
+                <div className="web-item2">
+                    <i className="devicon-django-line colored icon-skill"></i>
+                    <p>Django</p>
+                </div>
+            </div>
+        )
+    }
+}
