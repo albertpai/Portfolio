0bf253256e83a5cf68987d0c7f3e51fa9077e972
albertpai <verdant.light@gmail.com>
2018-08-14 00:31:23 +0100

    Change texts in container1Header.js

--- src/container1Header.js
+++ src/container1Header.js
@@ -5,11 +5,16 @@ export default class ContainerHeader extends React.Component {
     render() {
         return (
             <div>
-                <header className="container-head">
-                    <div className="App-header">
-                        <h1 className="App-title">
-                        Albert Pai's Portfolio
+                <header className="container-header">
+                    <div className="header-text">
+                        <h1 className="Title">
+                        Albert Pai
                         </h1>
+                        <h3 className="Subtitle">
+                        A web developer who always stands in your shoes
+                        <br/>
+                        Enquires at verdant.light@gmail.com
+                        </h3>
                         <a href="mailto:verdant.light@gmail.com?subject=Web Development Enquiry" target="_blank"><button className="button">Email Me</button></a>
                     </div>
                 </header>
