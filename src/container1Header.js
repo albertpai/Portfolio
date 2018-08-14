3409981d00eca465b33beb5869895d2767a69588
albertpai <verdant.light@gmail.com>
2018-08-14 01:22:41 +0100

    Rename class names

--- src/container1Header.js
+++ src/container1Header.js
@@ -7,15 +7,17 @@ export default class ContainerHeader extends React.Component {
             <div>
                 <header className="container-header">
                     <div className="header-text">
-                        <h1 className="Title">
+                        <h1 className="title">
                         Albert Pai
                         </h1>
-                        <h3 className="Subtitle">
+                        <h3 className="subtitle">
                         A web developer who always stands in your shoes
                         <br/>
                         Enquires at verdant.light@gmail.com
                         </h3>
-                        <a href="mailto:verdant.light@gmail.com?subject=Web Development Enquiry" target="_blank"><button className="button">Email Me</button></a>
+                        <a href="mailto:verdant.light@gmail.com?subject=Web Development Enquiry" target="_blank" rel="noopener noreferrer">
+                        <button className="button">Email Me</button>
+                        </a>
                     </div>
                 </header>
             </div>
