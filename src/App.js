f1a70db90c9af6d7f16c8f6eb0a8c25da9b8abe5
albertpai <verdant.light@gmail.com>
2018-08-30 05:33:41 +0100

    Remove ContainerIT in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ContainerHeader from './components/container1Header.js';
 import ContainerBio from './components/container2Bio.js';
 import ContainerProject from './components/container3Project.js';
 import ContainerWeb from './components/container4Web.js';
-// import ContainerIT from './components/container5IT.js';
 import ContainerMore from './components/container6More.js';
 import ContainerContact from './components/container7Contact.js';
 export default class App extends Component {
@@ -15,7 +14,6 @@ export default class App extends Component {
         <ContainerBio />
         <ContainerProject />
         <ContainerWeb />
-        {/* <ContainerIT /> */}
         <ContainerMore />
         <ContainerContact />
       </div>
