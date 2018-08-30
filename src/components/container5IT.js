e4c62442b874acec428b86e0e231d6acc5835a18
albertpai <verdant.light@gmail.com>
2018-08-30 04:19:01 +0100

    Change flexbox (Web & IT)

--- src/components/container5IT.js
+++ src/components/container5IT.js
@@ -4,25 +4,27 @@ import './container5IT.less';
 export default class ContainerIT extends React.Component {
     render() {
         return (
-                <div className="container-it">
+            <div className="container-it">
                 <h1 className="it-item1">IT Skills</h1>
-                    <div className="it-item2">
+                <div className="it-item2">
+                    <div className="it-item-icon">
                         <i className="devicon-slack-plain colored icon-skill-2"></i>
                         <p>Slack</p>
                     </div>
-                    <div className="it-item2">
+                    <div className="it-item-icon">
                         <i className="devicon-trello-plain colored icon-skill-2"></i>
                         <p>Trello</p>
                     </div>
-                    <div className="it-item2">
+                    <div className="it-item-icon">
                         <i className="devicon-visualstudio-plain colored icon-skill-2"></i>
                         <p>Visual Studio</p>
                     </div>
-                    <div className="it-item2">
+                    <div className="it-item-icon">
                         <i className="devicon-pycharm-plain colored icon-skill-2"></i>
                         <p>Pycharm</p>
                     </div>
                 </div>
+            </div>
         )
     }
 }
